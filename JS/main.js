b49824c8ceb002f8fc072763f04a86819052a06a
Seba9906 <troncoso.seb.99@gmail.com>
2023-05-25 23:01:55 -0300

    responsive-not finished

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,20 @@ window.addEventListener("resize", function() { //seleccionamos la regla nav del 
                                             con la cadena px especificamos que el valor debe ser interpretado como un numero de pixeles, este nro de pixeles sera la cantidad de espacio
                                             el cual nuestro margin top se desplazara*/
   });
+  window.onscroll = function (){miFuncion()};
+
+  var gato2 = document.getElementById("gato2");
+  var gato3 = document.getElementById("gato3");
+  
+  var distancia_gato2, distancia_gato3;
+  
+  function miFuncion(){
+      distancia_gato2 = window.innerHeight - gato2.getBoundingClientRect ().top;
+      if (distancia_gato2 >= 200){
+          gato2.classList.add("efecto-gato2");
+      }
+      distancia_gato3 = window.innerHeight - gato3.getBoundingClientRect ().top;
+      if (distancia_gato3 >= 200){
+          gato3.classList.add("efecto-gato3");
+      }
+  }
